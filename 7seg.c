cf0d0a7ef9a2445b1f68996c004782fdbe9d18d4
kumaraskanda <56293430+kumaraskanda@users.noreply.github.com>
2020-02-15 12:15:11 +0530

    Update 7seg.c

--- 7seg.c
+++ 7seg.c
@@ -1,101 +1,124 @@
-//Seven Segment Display Program: 
-//P0.19 Data pin of 1st shift register
-//P0.20 Clock pin of shift registers, make 1 to 0
-//P0.30 Strobe pin of shift registers: 1 to 0
-#include <lpc214x.h>
-#define LED_OFF (IO0SET = 1U << 31)
-#define LED_ON (IO0CLR = 1U << 31)
-#define PLOCK 0x00000400
-void delay_ms(unsigned int j);
-void SystemInit(void);
-unsigned char getAlphaCode(unsigned char alphachar);
-void alphadisp7SEG(char *buf);
-int main()
+#include <LPC214X.H>
+#include <stdio.h>
+#include <string.h>
+void delay(unsigned int j);
+#define PLOCK 0X00000400
+		
+char buf[6];
+		
+unsigned char dot()
 {
- IO0DIR |= 1U << 31 | 1U << 19 | 1U << 20 | 1U << 30 ; // to set as o/ps 
- LED_ON; // make D7 Led on .. just indicate the program is running
- SystemInit();
- while(1)
+	
+	switch(buf[4])
+	{
+		case '0': return 0x7f;
+		case '1': return 0x06;
+		case '2': return 0x24;
+		case '3': return 0x30;
+		case '4': return 0x19;
+		case '5': return 0x12;
+		case '6': return 0x02;
+		case '7': return 0x78;
+		case '8': return 0x00;
+		case '9': return 0x10;
+	}
+	return 0xff;
+}	
+	
+unsigned char get(unsigned char alpha)
 {
- alphadisp7SEG("fire ");
- delay_ms(500);
- alphadisp7SEG("help ");
- delay_ms(500);
+	switch(alpha)
+	{
+		case '0': return 0xc0;
+		case '1': return 0xf9;
+		case '2': return 0xa4;
+		case '3': return 0xb0;
+		case '4': return 0x99;
+		case '5': return 0x92;
+		case '6': return 0x82;
+		case '7': return 0xf8;
+		case '8': return 0x80;
+		case '9': return 0x90;
+		case 'f': return 0x8e;
+		case 'i': return 0xf9;
+		case 'r': return 0xce;
+		case 'e': return 0x86;
+		case 'h': return 0x89;
+		case 'l': return 0xc7;
+		case 'p': return 0x8c;
+		case ' ': return 0xff;
+		case '.': return 0x7f;
+		default:break;
+	}
+	return 0xff;
 }
-}
-unsigned char getAlphaCode(unsigned char alphachar)
-{ 
-switch (alphachar)
+void seg(char *buf)
 {
-// dp g f e d c b a - common anode: 0 segment on, 1 segment off
-case 'f':return 0x8e; 
-case 'i':return 0xf9;
-case 'r':return 0xce;
-case 'e':return 0x86; // 1000 0110
-case 'h':return 0x89;
-case 'l':return 0xc7;
-case 'p':return 0x8c;
-case ' ': return 0xff;
-//simmilarly add for other digit/characters
-default : break;
-}
-return 0xff;
-}
-void alphadisp7SEG(char *buf)
-{
-unsigned char i,j;
-unsigned char seg7_data,temp=0;
-for(i=0;i<5;i++) // because only 5 seven segment digits are present
- {
-seg7_data = getAlphaCode(*(buf+i)); //instead of this look up table can be used
- //to shift the segment data(8bits)to the hardware (shift registers) using Data,Clock,Strobe
-for (j=0 ; j<8; j++)
-{
-//get one bit of data for serial sending
-temp = seg7_data & 0x80; // shift data from Most significan bit (D7)
-if(temp == 0x80)
-IOSET0 |= 1 << 19; //IOSET0 | 0x00080000;
-else
-IOCLR0 |= 1 << 19; //IOCLR0 | 0x00080000;
-//send one clock pulse
-IOSET0 |= 1 << 20; //IOSET0 | 0x00100000;
- delay_ms(1); 
-IOCLR0 |= 1 << 20; //IOCLR0 | 0x00100000;
- seg7_data = seg7_data << 1; // get next bit into D7 position
-}
- }
+	unsigned char i,j;
+	unsigned char segment,temp=0;
+	
+	for(i=0;i<5;i++)
+	{
+		segment=get(*(buf+i));
+		for(j=0;j<8;j++)
+		{
+			temp=segment &0x80;
+			if(temp==0x80)
+				IOSET0|=1<<19;
+			else
+				IOCLR0|=1<<19;
+			
+			IOSET0|=1<<20;
+			delay(1);
+			IOCLR0|=1<<20;
+			segment=segment<<1;
+		}
+	}
+			IOSET0|=1<<30;
+			delay(1);
+			IOCLR0|=1<<30;
+			return;
+		}
 
-// send the strobe signal
-IOSET0 |= 1 << 30; //IOSET0 | 0x40000000;
-delay_ms(1); //nop();
-IOCLR0 |= 1 << 30; //IOCLR0 | 0x40000000;
-return;
- }
-void SystemInit(void)
+void dispInt(long int num)
 {
- PLL0CON = 0x01; 
- PLL0CFG = 0x24; 
- PLL0FEED = 0xAA; 
- PLL0FEED = 0x55; 
- while( !( PLL0STAT & PLOCK ))
- { ; }
- PLL0CON = 0x03;
- PLL0FEED = 0xAA; // lock the PLL registers after setting the required PLL
- PLL0FEED = 0x55;
- VPBDIV = 0x01; // PCLK is same as CCLK i.e 60Mhz 
+	char buf[6];
+	sprintf(buf,"%05lu",num);
+	seg(&buf[0]);
+	delay(500);
 }
-void delay_ms(unsigned int j)
+void dispFloat(float num)
 {
- unsigned int x,i;
- for(i=0;i<j;i++)
- {
- for(x=0; x<10000; x++); 
- }
+	sprintf(buf,"%05.2f",num);
+	seg(&buf[0]);
+	delay(500);
 }
 
+void systeminit(void)
+{
+	PLL0CON=0X01;
+	PLL0CFG=0X24;
+	PLL0FEED=0XAA;
+	PLL0FEED=0X55;
+	while(!(PLL0STAT & PLOCK))
+	{;}
+		PLL0CON=0X03;
+	PLL0FEED=0XAA;
+	PLL0FEED=0X55;
+	VPBDIV=0X01;
+	}
+void delay(unsigned int j)
+{
+	unsigned int x,i;
+	for(i=0;i<j;i++)
+	{
+	for(x=0;x<10000;x++);
+	}
+}
 
-// CODE to display an integer number/long integer number
-// long int dig_value;
-// unsigned char buf[5];
-// sprintf(buf,"%05lu",dig_value);
-// alphadisp7SEG(&buf[0]);
+		
+			
+			
+	
+
+
